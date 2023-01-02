cb189cc098b45290878923468a176dd4e674cc11
Nicola Barbieri <nico.gtf@gmail.com>
2023-01-02 08:36:48 +0100

    Scripts and Plots

---
+++ plot1.R
@@ -0,0 +1,21 @@
+# libraries
+library(dplyr)
+
+# read raw data
+df <- read.table('./data/household_power_consumption.txt', header = T, sep = ';')
+
+# convert columns to numbers
+df[,3:8] <- sapply(df[,3:8],as.numeric)
+
+# create a column datetime
+df$datetime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+
+# only rows between 2007-02-01 and 2007-02-02
+df <- filter(df, df$datetime>='2007-02-01 00:00:00' & df$datetime<'2007-02-03 00:00:00')
+
+# create plot in png file:
+png('plot1.png', height = 480, width = 480)
+with(df, hist(Global_active_power, col = 'red', xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power'))
+
+# close file
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,38 @@
+# libraries
+library(dplyr)
+
+# read raw data
+df <- read.table('./data/household_power_consumption.txt', header = T, sep = ';')
+
+# convert columns to numbers
+df[,3:8] <- sapply(df[,3:8],as.numeric)
+
+# create a column datetime
+df$datetime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+
+# only rows between 2007-02-01 and 2007-02-02
+df <- filter(df, df$datetime>='2007-02-01 00:00:00' & df$datetime<'2007-02-03 00:00:00')
+
+# create plot in png file:
+png('plot4.png', height = 480, width = 480)
+
+# 2 rows and 2 columns
+par(mfrow=c(2,2))
+
+# 1st chart
+with(df, plot(datetime, Global_active_power, type = 'l', xlab='', ylab = 'Global Active Power', main = ''))
+
+# 2nd chart
+with(df, plot(datetime, Voltage, type = 'l', xlab='datetime', ylab = 'Voltage', main = ''))
+
+# 3rd chart
+plot(df$datetime, df$Sub_metering_1, col = 'black', type = 'l', ylab = 'Energy sub metering', xlab = '')
+lines(df$datetime, df$Sub_metering_2, col = 'red', type = 'l')
+lines(df$datetime, df$Sub_metering_3, col = 'blue', type = 'l')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty = c(1,1,1))
+
+# 4th chart
+with(df, plot(datetime, Global_reactive_power, type = 'l', main = ''))
+
+# close file
+dev.off()
